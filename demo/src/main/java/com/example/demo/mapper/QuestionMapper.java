e44159c71fe30605a6f407da6a03114543b55d6b
kojima <shuran.han@morningstar.com>
2019-10-29 19:23:37 +0800

    Added the function to view topics in main page

--- demo/src/main/java/com/example/demo/mapper/QuestionMapper.java
+++ demo/src/main/java/com/example/demo/mapper/QuestionMapper.java
@@ -3,11 +3,16 @@ package com.example.demo.mapper;
 import com.example.demo.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
 public interface QuestionMapper {
     @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
     void create(Question question);
+    @Select("select * from Question")
+    List<Question> list();
 }
