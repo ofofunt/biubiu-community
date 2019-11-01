daf523e0395f50545e89510dfb0d8f179d83531f
kojima <shuran.han@morningstar.com>
2019-11-01 17:43:46 +0800

    Adding profile function (WIP2)\

--- demo/src/main/java/com/example/demo/controller/ProfileController.java
+++ demo/src/main/java/com/example/demo/controller/ProfileController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.PaginationDTO;
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.model.User;
+import com.example.demo.service.QuestionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -13,29 +16,43 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class ProfileController {
     @GetMapping("/profile/{action}")
-    public String profile(@PathVariable(name = "action") String action, Model model, HttpServletRequest request, UserMapper userMapper) {
+    public String profile(@PathVariable(name = "action") String action,
+                          Model model,
+                          HttpServletRequest request,
+                          UserMapper userMapper,
+                          @RequestParam(name = "page",defaultValue = "1") Integer page,
+                          @RequestParam(name = "size",defaultValue = "2") Integer size) {
         User user = null;
+        QuestionService questionService = new QuestionService();
         Cookie[] cookies = request.getCookies();
-        if (cookies !=null && cookies.length != 0) {
+        if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")){
+                if (cookie.getName().equals("token")) {
                     String token = cookie.getValue();
-                    user =userMapper.findByToken(token);
-                    if (user != null){
-                        request.getSession().setAttribute("user",user);
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
                     }
                     break;
                 }
             }
-        if ("questions".equals(action)) {
-            model.addAttribute("section", "questions");
-            model.addAttribute("sectionName","我的问题");
+            if (user == null){
+                return "redirect/";
+            }
+            if ("questions".equals(action)) {
+                model.addAttribute("section", "questions");
+                model.addAttribute("sectionName", "我的问题");
 
+            } else if ("replies".equals(action)) {
+                model.addAttribute("section", "replies");
+                model.addAttribute("sectionName", "最新回复");
+            }
+            return "profile";
         }
-        else if ("replies".equals(action)){
-            model.addAttribute("section","replies");
-            model.addAttribute("sectionName","最新回复");
-        }
+
+        PaginationDTO paginationDTO = questionService.list(user.getId(), page, size);
+        model.addAttribute("pagination", paginationDTO);
         return "profile";
+
     }
 }
